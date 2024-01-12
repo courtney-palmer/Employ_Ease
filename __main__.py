7dbc3b96fac88ebc74b9de5d97d1ba750257ffe7
courtney-palmer <courtneylpalmer@nevada.unr.edu>
2024-01-12 15:29:15 -0800

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,8 @@
+# EmployEase/__main__.py
+# from src.scripts.Main import main  # Import the main() function from Main.py
+from src.scripts.Main import main  # Import the main() function from Main.py
+def run_main():
+    main()  # Call the main() function from Main.py
+
+if __name__ == "__main__":
+    run_main()
